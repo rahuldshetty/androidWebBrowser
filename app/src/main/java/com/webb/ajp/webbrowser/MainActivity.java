21ec6367283cd6ed442b5de6e69363f278cec843
rahuldshetty <35rahuldshetty@gmail.com>
2018-09-20 19:13:50 +0530

    commit1

--- app/src/main/java/com/webb/ajp/webbrowser/MainActivity.java
+++ app/src/main/java/com/webb/ajp/webbrowser/MainActivity.java
@@ -2,12 +2,22 @@ package com.webb.ajp.webbrowser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar=findViewById(R.id.main_toolbar);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("");
+
     }
+
 }
